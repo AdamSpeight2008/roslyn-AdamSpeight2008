3f9b2a1653d4c9e5206e4be5da1dbc5620bfedc7
AdamSpeight2008 <adam.speight@virgin.net>
2018-03-16 06:24:39 +0000

    Making the right side of a DictionaryMemberLookup accept ExpressionSyntax rather then SimpleNameSyntax.

--- src/Compilers/VisualBasic/Test/CommandLine/CommandLineTests.vb
+++ src/Compilers/VisualBasic/Test/CommandLine/CommandLineTests.vb
@@ -1405,6 +1405,7 @@ End Module").Path
             parsedArgs = DefaultParse({"/langVERSION:latest", "a.vb"}, _baseDirectory)
             parsedArgs.Errors.Verify()
             Assert.Equal(LanguageVersion.Latest, parsedArgs.ParseOptions.SpecifiedLanguageVersion)
+            Assert.Equal(LanguageVersion.VisualBasic15_7, parsedArgs.ParseOptions.LanguageVersion)
             Assert.Equal(LanguageVersion.Latest.MapSpecifiedToEffectiveVersion(), parsedArgs.ParseOptions.LanguageVersion)
 
             ' default: "current version"
@@ -1921,7 +1922,7 @@ End Module").Path
             ' - update project-system to recognize the new value and pass it through
             ' - update all the tests that call this canary
             ' - update the command-line documentation (CommandLine.md)
-            AssertEx.SetEqual({"default", "9", "10", "11", "12", "14", "15", "15.3", "15.5", "16", "latest"},
+            AssertEx.SetEqual({"default", "9", "10", "11", "12", "14", "15", "15.3", "15.5", "15.7", "16" "latest"},
                 System.Enum.GetValues(GetType(LanguageVersion)).Cast(Of LanguageVersion)().Select(Function(v) v.ToDisplayString()))
             ' For minor versions, the format should be "x.y", such as "15.3"
         End Sub
@@ -1942,8 +1943,8 @@ End Module").Path
                 "15.0",
                 "15.3",
                 "15.5",
-                "16"
-             }
+                "15.7",
+                "16"             }
 
             AssertEx.SetEqual(versions, errorCodes)
 
@@ -1961,12 +1962,8 @@ End Module").Path
             Assert.Equal(LanguageVersion.VisualBasic15, LanguageVersion.VisualBasic15.MapSpecifiedToEffectiveVersion())
             Assert.Equal(LanguageVersion.VisualBasic15_3, LanguageVersion.VisualBasic15_3.MapSpecifiedToEffectiveVersion())
             Assert.Equal(LanguageVersion.VisualBasic15_5, LanguageVersion.VisualBasic15_5.MapSpecifiedToEffectiveVersion())
-            Assert.Equal(LanguageVersion.VisualBasic16, LanguageVersion.VisualBasic16.MapSpecifiedToEffectiveVersion())
-
-            Assert.Equal(LanguageVersion.VisualBasic15, LanguageVersion.Default.MapSpecifiedToEffectiveVersion())
-            Assert.Equal(LanguageVersion.VisualBasic15_5, LanguageVersion.Latest.MapSpecifiedToEffectiveVersion())
-
-            ' https//github.com/dotnet/roslyn/issues/29819 Once we are ready to remove the beta tag from VB 16 we should update Default/Latest accordingly
+            Assert.Equal(LanguageVersion.VisualBasic15_7, LanguageVersion.Latest.MapSpecifiedToEffectiveVersion())
+            Assert.Equal(LanguageVersion.VisualBasic16, LanguageVersion.VisualBasic16.MapSpecifiedToEffectiveVersion())            ' https//github.com/dotnet/roslyn/issues/29819 Once we are ready to remove the beta tag from VB 16 we should update Default/Latest accordingly
 
             ' The canary check is a reminder that this test needs to be updated when a language version is added
             LanguageVersionAdded_Canary()
@@ -1987,8 +1984,7 @@ End Module").Path
             InlineData("15.0", True, LanguageVersion.VisualBasic15),
             InlineData("15.3", True, LanguageVersion.VisualBasic15_3),
             InlineData("15.5", True, LanguageVersion.VisualBasic15_5),
-            InlineData("16", True, LanguageVersion.VisualBasic16),
-            InlineData("DEFAULT", True, LanguageVersion.Default),
+            InlineData("15.7", True, LanguageVersion.VisualBasic15_7),            InlineData("16", True, LanguageVersion.VisualBasic16),            InlineData("DEFAULT", True, LanguageVersion.Default),
             InlineData("default", True, LanguageVersion.Default),
             InlineData("LATEST", True, LanguageVersion.Latest),
             InlineData("latest", True, LanguageVersion.Latest),
