2e9e98362acb99d6012546996f52ff3aab06adcf
AdamSpeight2008 <adam.speight@virgin.net>
2019-01-11 17:07:19 +0000

    Tidy

--- src/Compilers/VisualBasic/Portable/Syntax/CrefOperatorReferenceSyntax.vb
+++ src/Compilers/VisualBasic/Portable/Syntax/CrefOperatorReferenceSyntax.vb
@@ -6,16 +6,14 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Syntax
 
   Partial Public Class CrefOperatorReferenceSyntax
 
-    <EditorBrowsable(EditorBrowsableState.Never)>
-    <Obsolete("This member is obsolete. Use OperatorKeyword or a more specific property (e.g. WithOperatorKeyword) instead.", True)>
+    <EditorBrowsable(EditorBrowsableState.Never), Obsolete("This member is obsolete. Use OperatorKeyword or a more specific property (e.g. WithOperatorKeyword) instead.", True)>
     Public ReadOnly Property Keyword As SyntaxToken
       Get
         Return OperatorKeyword
       End Get
     End Property
 
-    <EditorBrowsable(EditorBrowsableState.Never)>
-    <Obsolete("This member is obsolete. Use OperatorKeyword or a more specific property (e.g. WithOperatorKeyword) instead.", True)>
+    <EditorBrowsable(EditorBrowsableState.Never), Obsolete("This member is obsolete. Use OperatorKeyword or a more specific property (e.g. WithOperatorKeyword) instead.", True)>
     Public Function WithKeyword(keyword As SyntaxToken) As CrefOperatorReferenceSyntax
       Return WithOperatorKeyword(keyword)
     End Function
